60d116b16f31ab83e001a4273c52b66a126159a6
Mahmoud Abdelkader <mabdelkader@gmail.com>
2020-10-16 21:39:43 -0700

    Update docstring

--- larky/src/main/java/com/verygood/security/larky/annot/Library.java
+++ larky/src/main/java/com/verygood/security/larky/annot/Library.java
@@ -8,6 +8,8 @@ import java.lang.annotation.Target;
 /**
  * An annotation for a class that causes its StarlarkMethod-annotated methods to be predeclared in
  * the environment (by SkylarkParser) and added to the documentation (by MarkdownGenerator).
+ *
+ * See usage of <a href="https://github.com/google/copybara/blob/6b0c2867ad86059be4564611202f46fef9c8065e/java/com/google/copybara/CoreGlobal.java#L38>the Library annotation here</a>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
