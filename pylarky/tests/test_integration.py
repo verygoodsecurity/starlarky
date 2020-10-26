720cfe78234b9741da3c36441ae9aa87126a6cf1
Andrew <a.slepakurov@gmail.com>
2020-10-26 13:15:17 +0200

    PIP install (#4)
    * - Pip package for starlark interpreter
    - input/output parameters
    
    * Update access modifier on constructor
    
    * No-explicit file usage for lib
    Executable changes
    
    * Lib changes
    
    * Test for py lib
    
    * Test workflow
    
    * Python3 for venv
    
    * Typo
    
    * Exception test
    
    * Print error message, forse close tempfiles
    
    * Print stdout on error
    
    * Wrong exception. Fixed
    
    * Wrong exception. Fixed
    
    * Update pylarky/starlarky.py
    
    Co-authored-by: Petro Melnykov <petro.melnykov@vgs.io>
    
    * Update pylarky/starlarky.py
    
    Co-authored-by: Petro Melnykov <petro.melnykov@vgs.io>
    
    Co-authored-by: Petro Melnykov <petro.melnykov@vgs.io>

---
+++ pylarky/tests/test_integration.py
@@ -0,0 +1,37 @@
+import tempfile
+import pytest
+from pylarky.starlarky import evaluate, FailedEvaluation
+
+
+def test_evaluation():
+    starlark_script = """
+def modify():
+    return {"body": ctx["body"], "headers": {"accept": "json", "content": "still-json"}}
+modify()
+"""
+
+    script_file = tempfile.NamedTemporaryFile(mode='w+')
+    script_file.write(starlark_script)
+    script_file.flush()
+
+    eval_output = \
+        evaluate(script_file.name,
+                 'ctx = {"body": "thisisabody", "headers": {"accept": "xml", "content": "still-xml"}}')
+
+    assert eval_output == '{"body": "thisisabody", "headers": {"accept": "json", "content": "still-json"}}'
+
+
+def test_evaluation_exception():
+    starlark_script = """
+fun modify():
+    return {"body": ctx["body"], "headers": {"accept": "json", "content": "still-json"}}
+modify()
+    """
+
+    script_file = tempfile.NamedTemporaryFile(mode='w+')
+    script_file.write(starlark_script)
+    script_file.flush()
+
+    with pytest.raises(FailedEvaluation):
+        assert evaluate(script_file.name,
+                        'ctx = {"body": "thisisabody", "headers": {"accept": "xml", "content": "still-xml"}}')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pylarky',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.0.2',
+    description='Python wrapper for starlarky runner',
+    author='Very Good Security',
+    license='Apache License v2.0',
+    data_files=[('bin', ['larky-runner'])]
+)
