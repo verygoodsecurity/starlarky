21b410be5ca75599141e67899c2995eee1701431
Andrew <a.slepakurov@gmail.com>
2020-11-13 21:22:14 +0200

    Starlark update (#17)
    * Update Starlark to Bazel master.
    
    Update Bazel Hash: bazelbuild/bazel@fc27996
    
    starlark: remove StarlarkList.Builder
    starlark: make reversed() accept iterable, not just sequence
    starlark: change fail to def fail(*args), per spec
    starlark: eliminate one deprecated EvalException constructor
    starlark: more validity checks
    Use constraints in global conditions instead of configuration flags.
    starlark: simplify implementation of dict() and dict.update()
    starlark: rename ClassObject to Structure
    starlark: remove Dict.of(K, V) constructors
    Use StarlarkUtil::valueToStarlark, not fromJava
    starlark: clean up Dict construction
    starlark: add StarlarkList.Builder
    starlark: avoid unnecessary copies of frozen list/dicts
    starlark: avoid overloading of Dict.put and friends
    starlark: remove type parameter of Tuple
    starlark: move MethodLibrary.parseInt to StarlarkInt
    starlark eval: avoid ImmutableSortedMap, which is slow
    starlark: rename BuiltinCallable to BuiltinFunctionBazel
    
    * Update libstarlark with patches to let the tests compile
    
    * Update Larky with latest Starlark changes.
    
    - Update to use latest starlark
    - use StarlarkUtil.valueToStarlark instead of Starlark.fromJava
    
    Co-authored-by: Mahmoud Abdelkader <mabdelkader@gmail.com>

--- larky/src/main/java/com/verygood/security/larky/parser/StarlarkUtil.java
+++ larky/src/main/java/com/verygood/security/larky/parser/StarlarkUtil.java
@@ -152,25 +152,24 @@ public final class StarlarkUtil {
     return x == Starlark.NONE ? null : (String) x;
   }
 
-  public static Object valueToStarlark(Object x) throws EvalException {
+  public static Object valueToStarlark(Object x)  {
       // Is x a non-empty string_list_dict?
       if (x instanceof Map) {
-        Map<?, ?> map = (Map) x;
+        Map<?, ?> map = (Map<?,?>) x;
         if (!map.isEmpty() && map.values().iterator().next() instanceof List) {
           // Recursively convert subelements.
           Dict<Object, Object> dict = Dict.of(null);
           for (Map.Entry<?, ?> e : map.entrySet()) {
-            dict.put(
-              e.getKey(),
-              Starlark.fromJava(e.getValue(),null),
-              null);
+            try {
+              dict.putEntry(
+                  e.getKey(),
+                  Starlark.fromJava(e.getValue(),null));
+            } catch (EvalException evalException) {
+              throw new RuntimeException(evalException);
+            }
           }
           return dict;
         }
-      }
-
-      if(x instanceof String) {
-
       }
       // For all other attribute values, shallow conversion is safe.
       return Starlark.fromJava(x, null);
--- larky/src/main/java/com/verygood/security/larky/stdtypes/structs/MutableStruct.java
+++ larky/src/main/java/com/verygood/security/larky/stdtypes/structs/MutableStruct.java
@@ -2,7 +2,6 @@ package com.verygood.security.larky.stdtypes.structs;
 
 import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.EvalException;
-import net.starlark.java.syntax.Location;
 
 class MutableStruct extends SimpleStruct {
   MutableStruct(Dict<String, Object> fields) {
@@ -11,7 +10,7 @@ class MutableStruct extends SimpleStruct {
 
   @Override
   public void setField(String field, Object value) throws EvalException {
-    ((Dict<String, Object>) fields).put(field, value, (Location) null);
+    ((Dict<String, Object>) fields).putEntry(field, value);
   }
 
 }
