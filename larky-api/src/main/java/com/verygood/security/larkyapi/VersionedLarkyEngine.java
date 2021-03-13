63eb844a3ef83d37ff6b4d1c7fd25c200892e4b0
Mohamad El Hajj <mohamad.h.elhajj@gmail.com>
2021-03-12 20:22:14 -0500

    casting engine instance to ScriptEngine

--- larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngine.java
+++ larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngine.java
@@ -2,20 +2,21 @@ package com.verygood.security.larkyapi;
 
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
 
 public interface VersionedLarkyEngine extends ScriptEngine {
 
     Object executeScript(String script, String outputVar, ScriptContext context)
-            throws NoSuchMethodException,IllegalAccessException, InvocationTargetException;
+            throws NoSuchMethodException,IllegalAccessException, InvocationTargetException,ScriptException;
     Object executeScript(String script, String outputVar)
-            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException;
+            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException;
 
     Object executeScript(Reader script, String outputVar, ScriptContext context)
-            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException;
+            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException;
     Object executeScript(Reader script, String outputVar)
-            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException;
+            throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException;
 
     String getVersion();
 }
--- larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngineImpl.java
+++ larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngineImpl.java
@@ -21,7 +21,7 @@ public class VersionedLarkyEngineImpl implements VersionedLarkyEngine {
   private Class engineClass;
   private Class scriptClass;
   private Class parseClass;
-  private Object engineInstanceObj;
+  private ScriptEngine engineInstanceObj;
   private String version;
 
 
@@ -79,18 +79,17 @@ public class VersionedLarkyEngineImpl implements VersionedLarkyEngine {
             true, childLoader);
 
     // create engine object
-    this.engineInstanceObj = engineClass.newInstance();
+    this.engineInstanceObj = (ScriptEngine) engineClass.newInstance();
   }
 
   @Override
   public Object executeScript(String script,String outputVar)
-          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException {
+          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException {
 
     Method compile = engineClass.getMethod("compile",String.class);
-    Object compiledScript = compile.invoke(engineInstanceObj, (Object) script);
+    CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
 
-    Method eval = scriptClass.getMethod("eval");
-    Object starFile = eval.invoke(compiledScript);
+    Object starFile = compiledScript.eval();
 
     Method getGlblVar = parseClass.getMethod (
             "getGlobalEnvironmentVariable",
@@ -102,27 +101,26 @@ public class VersionedLarkyEngineImpl implements VersionedLarkyEngine {
 
   @Override
   public Object executeScript(String script,String outputVar, ScriptContext context)
-          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException {
+          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException {
     setContext(context);
     return executeScript(script, outputVar);
   }
 
   @Override
   public Object executeScript(Reader script, String outputVar, ScriptContext context)
-          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException  {
+          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException  {
     setContext(context);
     return executeScript(script, outputVar);
   }
 
   @Override
   public Object executeScript(Reader script, String outputVar)
-          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException  {
+          throws NoSuchMethodException,IllegalAccessException,InvocationTargetException,ScriptException  {
 
     Method compile = engineClass.getMethod("compile",Reader.class);
-    Object compiledScript = compile.invoke(engineInstanceObj, (Object) script);
+    CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
 
-    Method eval = scriptClass.getMethod("eval");
-    Object starFile = eval.invoke(compiledScript);
+    Object starFile = compiledScript.eval();
 
     Method getGlblVar = parseClass.getMethod(
             "getGlobalEnvironmentVariable",
@@ -145,197 +143,67 @@ public class VersionedLarkyEngineImpl implements VersionedLarkyEngine {
 
   @Override
   public Object eval(String script, ScriptContext context) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",String.class,ScriptContext.class)
-              .invoke(engineInstanceObj, script, context);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(script,context);
   }
 
   @Override
   public Object eval(Reader reader, ScriptContext context) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",Reader.class,ScriptContext.class)
-              .invoke(engineInstanceObj, reader, context);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(reader,context);
   }
 
   @Override
   public Object eval(String script) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",String.class)
-              .invoke(engineInstanceObj, script);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(script);
   }
 
   @Override
   public Object eval(Reader reader) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",Reader.class)
-              .invoke(engineInstanceObj, reader);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(reader);
   }
 
   @Override
   public Object eval(String script, Bindings n) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",String.class,Bindings.class)
-              .invoke(engineInstanceObj, script, n);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(script,n);
   }
 
   @Override
   public Object eval(Reader reader, Bindings n) throws ScriptException {
-    try {
-      return engineClass.getMethod("eval",Reader.class,Bindings.class)
-              .invoke(engineInstanceObj, reader, n);
-    } catch (InvocationTargetException ite) {
-      if (ite.getCause() instanceof ScriptException) {
-        throw (ScriptException) ite.getCause();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.eval(reader,n);
   }
 
   @Override
   public void put(String key, Object value) {
-    try {
-      engineClass.getMethod("put",String.class,Object.class)
-              .invoke(engineInstanceObj, key, value);
-    } catch (Exception e) {
-      if (e instanceof InvocationTargetException) {
-        if (e.getCause() instanceof NullPointerException) {
-          throw (NullPointerException) e.getCause();
-        } else if (e.getCause() instanceof IllegalArgumentException) {
-          throw (IllegalArgumentException) e.getCause();
-        }
-      }
-      e.printStackTrace();
-    }
+    engineInstanceObj.put(key,value);
   }
 
   @Override
   public Object get(String key) {
-    try {
-      return engineClass.getMethod("get",String.class)
-              .invoke(engineInstanceObj, key);
-    } catch (Exception e) {
-      if (e instanceof InvocationTargetException) {
-        if (e.getCause() instanceof NullPointerException) {
-          throw (NullPointerException) e.getCause();
-        } else if (e.getCause() instanceof IllegalArgumentException) {
-          throw (IllegalArgumentException) e.getCause();
-        }
-      }
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.get(key);
   }
 
   @Override
   public Bindings getBindings(int scope) {
-    try {
-      return (Bindings) engineClass.getMethod("getBindings", int.class)
-              .invoke(engineInstanceObj, scope);
-    } catch (Exception e) {
-      if (e instanceof InvocationTargetException) {
-        if (e.getCause() instanceof IllegalArgumentException) {
-          throw (IllegalArgumentException) e.getCause();
-        }
-      }
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.getBindings(scope);
   }
 
   @Override
   public void setBindings(Bindings bindings, int scope) {
-    try {
-      engineClass.getMethod("setBindings", Bindings.class, int.class)
-              .invoke(engineInstanceObj, bindings, scope);
-    } catch (Exception e) {
-      if (e instanceof InvocationTargetException) {
-        if (e.getCause() instanceof NullPointerException) {
-          throw (NullPointerException) e.getCause();
-        } else if (e.getCause() instanceof IllegalArgumentException) {
-          throw (IllegalArgumentException) e.getCause();
-        }
-      }
-      e.printStackTrace();
-    }
+    engineInstanceObj.setBindings(bindings,scope);
   }
 
   @Override
   public Bindings createBindings() {
-    try {
-      return (Bindings) engineClass.getMethod("createBindings")
-              .invoke(engineInstanceObj);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.createBindings();
   }
 
   @Override
   public ScriptContext getContext() {
-    try {
-      return (ScriptContext) engineClass.getMethod("getContext")
-              .invoke(engineInstanceObj);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return engineInstanceObj.getContext();
   }
 
   @Override
   public void setContext(@Nonnull ScriptContext context) {
-    try {
-      engineClass.getMethod("setContext", ScriptContext.class)
-              .invoke(engineInstanceObj, context);
-    } catch (Exception e) {
-      if (e instanceof InvocationTargetException) {
-        if (e.getCause() instanceof NullPointerException) {
-          throw (NullPointerException) e.getCause();
-        }
-      }
-      e.printStackTrace();
-    }
+    engineInstanceObj.setContext(context);
   }
 
   @Override
