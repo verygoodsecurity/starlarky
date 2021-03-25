da1e29761fd4bbe07025301d3f2e2fc52ff759b6
Mahmoud Abdelkader <mabdelkader@gmail.com>
2021-03-24 21:40:18 -0700

    Start implementation of Pycryptodome using bouncycastle.

---
+++ larky/src/main/java/com/verygood/security/larky/modules/crypto/CryptoIOModule.java
@@ -0,0 +1,9 @@
+package com.verygood.security.larky.modules.crypto;
+
+import net.starlark.java.eval.StarlarkValue;
+
+public class CryptoIOModule implements StarlarkValue {
+
+  public static final CryptoIOModule INSTANCE = new CryptoIOModule();
+
+}
---
+++ larky/src/main/java/com/verygood/security/larky/modules/crypto/CryptoProtocolModule.java
@@ -0,0 +1,9 @@
+package com.verygood.security.larky.modules.crypto;
+
+import net.starlark.java.eval.StarlarkValue;
+
+public class CryptoProtocolModule implements StarlarkValue {
+
+  public static final CryptoProtocolModule INSTANCE = new CryptoProtocolModule();
+
+}
