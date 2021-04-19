ea56fd94c8c5bb72554dbc25bbf48c8f94297426
Mohamad Hadi El Hajj <19651160+moehajj@users.noreply.github.com>
2021-04-19 15:52:32 -0400

    Changing larky-api jar loading methodology (#101)
    * changed jar loading strategy
    
    * updated circleci config
    
    * added readme
    
    * cleaned dependencies
    
    * changed default lib directory
    
    * changed default lib directory
    
    * updated circleci route
    
    * updated readme
    
    * added logging
    
    * fixed file formatting
    
    * removed snake_case
    
    * precompiled pattern only once
    
    * remove macos from workflow
    
    * refactored impl
    
    * undo circleci changes

--- larky-api/scripts/get_resources.sh
+++ larky-api/scripts/get_resources.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
-if [[ -z "${LARKY_HOME}" ]]; then
-  echo "LARKY_HOME not found"
-  exit 1
+# set default verbose level to warning
+__VERBOSE=4
+
+# Setup Logging Infra
+declare LOG_LEVELS
+LOG_LEVELS=([0]="emerg" [1]="alert" [2]="crit" [3]="err" [4]="warning" [5]="notice" [6]="info" [7]="debug")
+function .log () {
+  local LEVEL=${1}
+  shift
+  if [ ${__VERBOSE} -ge ${LEVEL} ]; then
+    echo "[${LOG_LEVELS[$LEVEL]}]" "$@"
+  fi
+}
+
+# Read flags from command line
+while getopts ":dis" o; do
+    case "${o}" in
+        d) #debug
+            __VERBOSE=7
+            ;;
+        i) #debug
+            __VERBOSE=6
+            ;;
+        s) #silent
+            __VERBOSE=3
+            ;;
+        *)
+          ;;
+    esac
+done
+.log 7 "VERBOSE levels set to [${LOG_LEVELS[$__VERBOSE]}]"
+
+
+# Get larky lib home path
+LARKY_LIB_HOME=${LARKY_LIB_HOME:-~/.larky/lib}
+mkdir -p $LARKY_LIB_HOME
+if [ $? -ne 0 ] ; then
+    .log 3 "could not create directory $LARKY_LIB_HOME"
+    exit
 fi
 
 # Construct GraphQL query to get Larky package versions and jar files
@@ -29,8 +65,10 @@ gql_query='query {
 
 # Get packages from github registry using GraphQL API
 # Parse and clean output into {"packages": [ {v1,jar1},{v2,jar2},...,{vN,jarN} ] }
+.log 6 "Downloading from $url into $LARKY_API_JAR"
 package_json=$(
-  curl  -H 'Content-Type: application/json' \
+  curl  $( (( __VERBOSE < 7 )) && printf %s '-s' ) \
+        -H 'Content-Type: application/json' \
         -H "Authorization: bearer $GITHUB_API_TOKEN" \
         -X POST \
         -d "{\"query\": \"$(echo $gql_query)\"}" \
@@ -50,16 +88,16 @@ package_json=$(
 )
 
 # Download fat jar files from github registry
-API_RESOURCE_HOME=$LARKY_HOME/larky-api/src/main/resources
 echo $package_json | jq -c '.packages[]'| while read i; do
     # get cleaned verion & jar name
     version=$(jq ".version" <<< $i | sed -e 's/^"//' -e 's/"$//')
     url=$(jq ".url" <<< $i | sed -e 's/^"//' -e 's/"$//')
 
     # construct output jar paths
-    LARKY_API_JAR=$API_RESOURCE_HOME/larky-$version-fat.jar
+    LARKY_API_JAR=$LARKY_LIB_HOME/larky-$version-fat.jar
 
     # get jar
-    curl -o $LARKY_API_JAR -L $url
+    .log 6 "Downloading from $url into $LARKY_API_JAR"
+    curl $( (( __VERBOSE < 7 )) && printf %s '-s' ) -o $LARKY_API_JAR -L $url
 
 done
--- larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngineImpl.java
+++ larky-api/src/main/java/com/verygood/security/larkyapi/VersionedLarkyEngineImpl.java
@@ -1,227 +1,245 @@
 package com.verygood.security.larkyapi;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.Nonnull;
 import javax.script.CompiledScript;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 import javax.script.Bindings;
 import javax.script.ScriptEngineFactory;
+import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
+@Slf4j
 public class VersionedLarkyEngineImpl implements VersionedLarkyEngine {
 
-  final private Class engineClass;
-  final private Class scriptClass;
-  final private Class parseClass;
-  final private ScriptEngine engineInstanceObj;
-  final private String version;
+    final private Class engineClass;
+    final private Class scriptClass;
+    final private Class parseClass;
+    final private ScriptEngine engineInstanceObj;
+    final private String version;
+
+    private static IOException setupException = null;
 
 
-  // Instantiating the static map
-  private static Map<String, URL> larkyJarByVersion;
-  static {
-    detectVersions();
-  }
+    // Instantiating the static map
+    private static Map<String, URL> larkyJarByVersion;
 
-  private static void detectVersions() {
-    larkyJarByVersion = new HashMap<>();
-    try {
-      PathMatchingResourcePatternResolver resolver =
-              new PathMatchingResourcePatternResolver(VersionedLarkyEngineImpl.class.getClassLoader());
-      // Get jars with format `larky-\d{>=1}.\d{>=1}.\d{>=1}-fat.jar`
-      Resource[] resources = resolver.getResources( // uses AntPathMatcher
-              "classpath*:larky-{\\d+}.{\\d+}.{\\d+}-fat.jar"
-      );
-      for (Resource resource: resources) {
-        String fileName = resource.getFilename();
-        URL fileURL = resource.getURL();
+    static {
+        detectVersions();
+    }
+
+    private static void detectVersions() {
+        larkyJarByVersion = new HashMap<>();
+
+        String larkyLibEnv = System.getenv("LARKY_LIB_HOME");
+        String larkyLib = larkyLibEnv != null ?
+                larkyLibEnv :
+                String.format("%s/.larky/lib", System.getProperty("user.home"));
 
         Pattern pattern = Pattern.compile("\\d+.\\d+.\\d+");
-        Matcher matcher = pattern.matcher(fileName);
-        if (matcher.find()) {
-          larkyJarByVersion.put(matcher.group(), fileURL);
+        try {
+            Stream<Path> paths = Files.walk(Paths.get(larkyLib));
+            paths
+                    // Get jars with format `larky-\d{>=1}.\d{>=1}.\d{>=1}-fat.jar`
+                    .filter(filePath -> filePath.toString().matches("^.*larky-\\d+.\\d+.\\d+-fat.jar$"))
+                    .forEach(filePath -> {
+                        try {
+                            String fileName = filePath.toString();
+                            URL fileURL = filePath.toUri().toURL();
+
+                            Matcher matcher = pattern.matcher(fileName);
+                            if (matcher.find()) {
+                                larkyJarByVersion.put(matcher.group(), fileURL);
+                            }
+                        } catch (Exception e) {
+                            log.error("Failed to extract jar file from URL, due to {}", e.getMessage());
+                        }
+                    });
+        } catch (IOException e) {
+            setupException = e;
+            log.error("Unable to resolve jar files in path {}, due to {}", larkyLib, e.getMessage());
         }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  public VersionedLarkyEngineImpl(String inputVersion)
-          throws IllegalArgumentException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-
-    if ( !getSupportedVersions().contains(inputVersion) ) {
-      throw new IllegalArgumentException("Engine Version not Found");
     }
 
-    this.version = inputVersion;
-    URL larkyJarPath = larkyJarByVersion.get(version);
-    URLClassLoader childLoader = new URLClassLoader(
-            new URL[] {larkyJarPath},
-            VersionedLarkyEngineImpl.class.getClassLoader()
-    );
+    public VersionedLarkyEngineImpl(String inputVersion)
+            throws IllegalArgumentException, ClassNotFoundException, IllegalAccessException, InstantiationException,
+            IOException {
 
-    // equivalent to: import com.verygood.security.larky.jsr223.LarkyCompiledScript;
-    this.engineClass = Class.forName(
-            "com.verygood.security.larky.jsr223.LarkyScriptEngine",
-            true, childLoader);
+        if (setupException != null) {
+            throw setupException;
+        }
 
-    // equivalent to: import com.verygood.security.larky.jsr223.LarkyScriptEngine;
-    this.scriptClass = Class.forName(
-            "com.verygood.security.larky.jsr223.LarkyCompiledScript",
-            true, childLoader);
+        if (!getSupportedVersions().contains(inputVersion)) {
+            throw new IllegalArgumentException("Engine Version not Found");
+        }
 
-    // equivalent to: import com.verygood.security.larky.parser.ParsedStarFile;
-    this.parseClass = Class.forName(
-            "com.verygood.security.larky.parser.ParsedStarFile",
-            true, childLoader);
+        this.version = inputVersion;
+        URL larkyJarPath = larkyJarByVersion.get(version);
+        URLClassLoader childLoader = new URLClassLoader(
+                new URL[]{larkyJarPath}
+        );
 
-    // create engine object
-    this.engineInstanceObj = (ScriptEngine) engineClass.newInstance();
-  }
+        // equivalent to: import com.verygood.security.larky.jsr223.LarkyCompiledScript;
+        this.engineClass = Class.forName(
+                "com.verygood.security.larky.jsr223.LarkyScriptEngine",
+                true, childLoader);
 
-  @Override
-  public Object executeScript(String script, String outputVar)
-          throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
-          ScriptException, NullPointerException {
+        // equivalent to: import com.verygood.security.larky.jsr223.LarkyScriptEngine;
+        this.scriptClass = Class.forName(
+                "com.verygood.security.larky.jsr223.LarkyCompiledScript",
+                true, childLoader);
 
-    Method compile = engineClass.getMethod("compile", String.class);
-    CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
+        // equivalent to: import com.verygood.security.larky.parser.ParsedStarFile;
+        this.parseClass = Class.forName(
+                "com.verygood.security.larky.parser.ParsedStarFile",
+                true, childLoader);
 
-    Object starFile = compiledScript.eval();
+        // create engine object
+        this.engineInstanceObj = (ScriptEngine) engineClass.newInstance();
+    }
 
-    Method getGlblVar = parseClass.getMethod(
-            "getGlobalEnvironmentVariable",
-            String.class, Class.class);
-    Object result = getGlblVar.invoke(starFile, outputVar, Object.class);
+    @Override
+    public Object executeScript(String script, String outputVar)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+            ScriptException, NullPointerException {
 
-    return result;
-  }
+        Method compile = engineClass.getMethod("compile", String.class);
+        CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
 
-  @Override
-  public Object executeScript(String script, String outputVar, ScriptContext context)
-          throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException {
-    setContext(context);
-    return executeScript(script, outputVar);
-  }
+        Object starFile = compiledScript.eval();
 
-  @Override
-  public Object executeScript(Reader script, String outputVar, ScriptContext context)
-          throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException  {
-    setContext(context);
-    return executeScript(script, outputVar);
-  }
+        Method getGlblVar = parseClass.getMethod(
+                "getGlobalEnvironmentVariable",
+                String.class, Class.class);
+        Object result = getGlblVar.invoke(starFile, outputVar, Object.class);
 
-  @Override
-  public Object executeScript(Reader script, String outputVar)
-          throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException  {
+        return result;
+    }
 
-    Method compile = engineClass.getMethod("compile", Reader.class);
-    CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
+    @Override
+    public Object executeScript(String script, String outputVar, ScriptContext context)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException {
+        setContext(context);
+        return executeScript(script, outputVar);
+    }
 
-    Object starFile = compiledScript.eval();
+    @Override
+    public Object executeScript(Reader script, String outputVar, ScriptContext context)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException {
+        setContext(context);
+        return executeScript(script, outputVar);
+    }
 
-    Method getGlblVar = parseClass.getMethod(
-            "getGlobalEnvironmentVariable",
-            String.class, Class.class);
-    Object result = getGlblVar.invoke(starFile, outputVar, Object.class);
+    @Override
+    public Object executeScript(Reader script, String outputVar)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ScriptException {
 
-    return result;
-  }
+        Method compile = engineClass.getMethod("compile", Reader.class);
+        CompiledScript compiledScript = (CompiledScript) compile.invoke(engineInstanceObj, script);
 
-  public static Set<String> getSupportedVersions() {
-    return larkyJarByVersion.keySet();
-  }
+        Object starFile = compiledScript.eval();
 
-  @Override
-  public String getVersion() {
-    return version;
-  }
+        Method getGlblVar = parseClass.getMethod(
+                "getGlobalEnvironmentVariable",
+                String.class, Class.class);
+        Object result = getGlblVar.invoke(starFile, outputVar, Object.class);
+
+        return result;
+    }
+
+    public static Set<String> getSupportedVersions() {
+        return larkyJarByVersion.keySet();
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
 
 
-  @Override
-  public Object eval(String script, ScriptContext context) throws ScriptException {
-    return engineInstanceObj.eval(script, context);
-  }
+    @Override
+    public Object eval(String script, ScriptContext context) throws ScriptException {
+        return engineInstanceObj.eval(script, context);
+    }
 
-  @Override
-  public Object eval(Reader reader, ScriptContext context) throws ScriptException {
-    return engineInstanceObj.eval(reader, context);
-  }
+    @Override
+    public Object eval(Reader reader, ScriptContext context) throws ScriptException {
+        return engineInstanceObj.eval(reader, context);
+    }
 
-  @Override
-  public Object eval(String script) throws ScriptException {
-    return engineInstanceObj.eval(script);
-  }
+    @Override
+    public Object eval(String script) throws ScriptException {
+        return engineInstanceObj.eval(script);
+    }
 
-  @Override
-  public Object eval(Reader reader) throws ScriptException {
-    return engineInstanceObj.eval(reader);
-  }
+    @Override
+    public Object eval(Reader reader) throws ScriptException {
+        return engineInstanceObj.eval(reader);
+    }
 
-  @Override
-  public Object eval(String script, Bindings n) throws ScriptException {
-    return engineInstanceObj.eval(script, n);
-  }
+    @Override
+    public Object eval(String script, Bindings n) throws ScriptException {
+        return engineInstanceObj.eval(script, n);
+    }
 
-  @Override
-  public Object eval(Reader reader, Bindings n) throws ScriptException {
-    return engineInstanceObj.eval(reader, n);
-  }
+    @Override
+    public Object eval(Reader reader, Bindings n) throws ScriptException {
+        return engineInstanceObj.eval(reader, n);
+    }
 
-  @Override
-  public void put(String key, Object value) {
-    engineInstanceObj.put(key, value);
-  }
+    @Override
+    public void put(String key, Object value) {
+        engineInstanceObj.put(key, value);
+    }
 
-  @Override
-  public Object get(String key) {
-    return engineInstanceObj.get(key);
-  }
+    @Override
+    public Object get(String key) {
+        return engineInstanceObj.get(key);
+    }
 
-  @Override
-  public Bindings getBindings(int scope) {
-    return engineInstanceObj.getBindings(scope);
-  }
+    @Override
+    public Bindings getBindings(int scope) {
+        return engineInstanceObj.getBindings(scope);
+    }
 
-  @Override
-  public void setBindings(Bindings bindings, int scope) {
-    engineInstanceObj.setBindings(bindings, scope);
-  }
+    @Override
+    public void setBindings(Bindings bindings, int scope) {
+        engineInstanceObj.setBindings(bindings, scope);
+    }
 
-  @Override
-  public Bindings createBindings() {
-    return engineInstanceObj.createBindings();
-  }
+    @Override
+    public Bindings createBindings() {
+        return engineInstanceObj.createBindings();
+    }
 
-  @Override
-  public ScriptContext getContext() {
-    return engineInstanceObj.getContext();
-  }
+    @Override
+    public ScriptContext getContext() {
+        return engineInstanceObj.getContext();
+    }
 
-  @Override
-  public void setContext(@Nonnull ScriptContext context) {
-    engineInstanceObj.setContext(context);
-  }
+    @Override
+    public void setContext(ScriptContext context) {
+        engineInstanceObj.setContext(context);
+    }
 
-  @Override
-  public ScriptEngineFactory getFactory() {
-    return null;
-  }
+    @Override
+    public ScriptEngineFactory getFactory() {
+        return null;
+    }
 
 }
