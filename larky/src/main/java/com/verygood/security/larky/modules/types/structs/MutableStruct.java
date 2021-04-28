b0d3dd23e67fce6369bd920cf052bdca53c7cd5d
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2021-04-28 02:28:15 -0700

    Very basic support for X509 support to mimic pyOpenSSL crypto module (#106)
    * [WIP] Test is working and all is passing. Allows RSA keys to be used to dump a X509 certificate. Very basic support enabled.
    
    Also added `__dict__` support for MutableStructs.
    
    Also added `setattr()` in python built-ins, though I do not think this will be needed.
    
    * Minor

--- larky/src/main/java/com/verygood/security/larky/modules/types/structs/MutableStruct.java
+++ larky/src/main/java/com/verygood/security/larky/modules/types/structs/MutableStruct.java
@@ -3,9 +3,13 @@ package com.verygood.security.larky.modules.types.structs;
 import com.google.common.base.Joiner;
 
 import com.verygood.security.larky.modules.types.Property;
+import com.verygood.security.larky.modules.types.PyProtocols;
 
+import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.EvalException;
+import net.starlark.java.eval.Starlark;
+import net.starlark.java.eval.StarlarkList;
 import net.starlark.java.eval.StarlarkThread;
 
 import lombok.SneakyThrows;
@@ -14,6 +18,22 @@ class MutableStruct extends SimpleStruct {
   MutableStruct(Dict<String, Object> fields, StarlarkThread currentThread) {
     super(fields, currentThread);
   }
+
+  @StarlarkMethod(name = PyProtocols.__DICT__, useStarlarkThread = true)
+  public Dict<String, Object> dunderDict(StarlarkThread thread) throws EvalException {
+    StarlarkList<String> keys = Starlark.dir(thread.mutability(), thread.getSemantics(), this);
+    Dict.Builder<String, Object> builder = Dict.builder();
+    for(String k : keys) {
+      // obviously, ignore the actual __dict__ key since we're in this method already
+      if(k.equals(PyProtocols.__DICT__)) {
+        continue;
+      }
+      Object value = super.getValue(k);
+      builder.put(k,  value != null ? value : Starlark.NONE);
+    }
+    return builder.build(thread.mutability());
+  }
+
 
   @Override
   public Object getValue(String name) throws EvalException {
@@ -35,6 +55,9 @@ class MutableStruct extends SimpleStruct {
 
   @Override
   public void setField(String name, Object value) throws EvalException {
+    if(this.currentThread.mutability().isFrozen()) {
+      throw new EvalException("Attempting to update a frozen structure");
+    }
     Object field = this.fields.get(name);
     /* if we have assigned a field that is a descriptor, we can invoke it */
     if (field == null
