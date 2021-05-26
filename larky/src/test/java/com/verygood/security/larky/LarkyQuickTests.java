8014c3ae4e8a51c036a0863d427ec489fa002e6f
aslepakurov <a.slepakurov@gmail.com>
2021-05-26 14:06:52 +0300

    Rollback test

--- larky/src/test/java/com/verygood/security/larky/LarkyQuickTests.java
+++ larky/src/test/java/com/verygood/security/larky/LarkyQuickTests.java
@@ -56,8 +56,7 @@ public class LarkyQuickTests {
   private List<Path> enumerateTests() {
     // Did we pass in a specific filename?
     // -Dlarky.quick_test=test_base64.star
-//    String singleTestDesired = System.getProperty(PROPERTY_NAME);
-    String singleTestDesired = "test_jwe.star";
+    String singleTestDesired = System.getProperty(PROPERTY_NAME);
     try (Stream<Path> testFiles = Files.walk(QUICK_TEST_DIR)) {
       scratchTestFiles = testFiles
           .filter(Files::isRegularFile)
