26c23b57d0f8bb0629009ae8a22bbddf352309af
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2021-05-27 09:25:15 -0700

    [READY] ✅  -- Introduce result type (#121)
    * Introduce result type
    
    ```python
    load("@stdlib//jresult", _jresult="jresult")
    
    f = _jresult.Result.success(1)
    print(f)
    print("-----")
    print(f.map(lambda x: x+x).value())
    print(f.is_ok == True)
    print(f.is_err == False)
    ```
    
    output:
    
    ```
    0520 19:19:23.134 INFO: stdlib/larky.star:31:6: Result{value=1, error=null}
    0520 19:19:23.134 INFO: stdlib/larky.star:32:6: -----
    0520 19:19:23.139 INFO: stdlib/larky.star:33:6: 2
    0520 19:19:23.140 INFO: stdlib/larky.star:34:6: True
    0520 19:19:23.140 INFO: stdlib/larky.star:35:6: True
    ```
    
    And:
    
    ```python
    load("@stdlib//jresult", _jresult="jresult")
    
    d = _jresult.Result.failure("oh no!")
    print(d)
    print(d.is_ok == False)
    print(d.is_err == True)
    ```
    
    output:
    
    ```
    0520 19:19:23.143 INFO: stdlib/larky.star:37:6: Result{value=null, error=oh no!}
    0520 19:19:23.144 INFO: stdlib/larky.star:38:6: True
    0520 19:19:23.144 INFO: stdlib/larky.star:39:6: True
    ```
    
    * Add the operator module to simplify py2star migration
    
    * Allow parametrized tests!
    
    * Introduce an Ok and Error object
    
    Taking a suggestion out of @Agitolyev review, I have introduced an Ok and Error
    object.
    
    Resolves this comment:
    https://github.com/verygoodsecurity/starlarky/pull/121#discussion_r636860293
    
    * more tests and introduce operator tests
    
    * More tests for the operator module
    
    * more operator tests
    
    * checkpoint
    
    * all tests are passing
    
    * remove unused comment
    
    * StarlarkException vs EvalException?
    
    * all tests are passing @_@
    
    * Add the safe() decorator
    
    * cleanup unused comment
    
    * Update larky/src/main/java/com/verygood/security/larky/modules/ResultModule.java
    
    Co-authored-by: Andrew <a.slepakurov@gmail.com>
    
    * add flatmap
    
    Co-authored-by: Andrew <a.slepakurov@gmail.com>

--- larky/src/main/java/com/verygood/security/larky/modules/globals/PythonBuiltins.java
+++ larky/src/main/java/com/verygood/security/larky/modules/globals/PythonBuiltins.java
@@ -1,11 +1,19 @@
 package com.verygood.security.larky.modules.globals;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+
 import com.verygood.security.larky.annot.Library;
 import com.verygood.security.larky.modules.codecs.TextUtil;
 import com.verygood.security.larky.modules.types.LarkyByte;
 import com.verygood.security.larky.modules.types.LarkyByteArray;
 import com.verygood.security.larky.modules.types.LarkyByteLike;
 import com.verygood.security.larky.modules.types.LarkyObject;
+import com.verygood.security.larky.modules.types.PyProtocols;
 import com.verygood.security.larky.parser.StarlarkUtil;
 
 import net.starlark.java.annot.Param;
@@ -25,13 +33,6 @@ import net.starlark.java.eval.Structure;
 import net.starlark.java.eval.Tuple;
 
 import org.apache.commons.text.translate.CharSequenceTranslator;
-
-import java.math.BigInteger;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
-import java.nio.charset.StandardCharsets;
-import java.nio.charset.UnsupportedCharsetException;
 
 
 /**
@@ -121,6 +122,45 @@ public final class PythonBuiltins {
     }
     return StarlarkInt.of(new BigInteger(bytes).intValueExact());
   }
+
+  //override built-in getattr
+
+  @StarlarkMethod(
+       name = "getattr",
+       doc =
+           "Returns the struct's field of the given name if it exists. If not, it either returns "
+               + "<code>default</code> (if specified) or raises an error. "
+               + "<code>getattr(x, \"foobar\")</code> is equivalent to <code>x.foobar</code>."
+               + "<pre class=\"language-python\">getattr(ctx.attr, \"myattr\")\n"
+               + "getattr(ctx.attr, \"myattr\", \"mydefault\")</pre>",
+       parameters = {
+         @Param(name = "x", doc = "The struct whose attribute is accessed."),
+         @Param(name = "name", doc = "The name of the struct attribute."),
+         @Param(
+             name = "default",
+             defaultValue = "unbound",
+             doc =
+                 "The default value to return in case the struct "
+                     + "doesn't have an attribute of the given name.")
+       },
+       useStarlarkThread = true)
+   public Object getattr(Object obj, String name, Object defaultValue, StarlarkThread thread)
+       throws EvalException, InterruptedException {
+    if (LarkyObject.class.isAssignableFrom(obj.getClass())) {
+      // if there's an object with a __getattr__, it will be invoked..
+      Object getAttrMethod = ((LarkyObject) obj).getField(PyProtocols.__GETATTR__);
+      if (getAttrMethod != null) {
+        Object res = Starlark.call(thread, getAttrMethod, Tuple.of(name), Dict.empty());
+        return (res != null) ? res : defaultValue;
+      }
+    }
+     return Starlark.getattr(
+         thread.mutability(),
+         thread.getSemantics(),
+         obj,
+         name,
+         defaultValue == Starlark.UNBOUND ? null : defaultValue);
+   }
 
   //override built-in type
   @StarlarkMethod(
@@ -272,7 +312,7 @@ public final class PythonBuiltins {
           // fallthrough
           return StarlarkFloat.of(Math.abs(((StarlarkFloat) x).toDouble()));
         default:
-          throw Starlark.errorf("bad operand type for abs(): '%s'", classType);
+          throw Starlark.errorf("TypeError: bad operand type for abs(): '%s'", classType);
       }
     } catch (EvalException | ClassCastException ex) {
       throw Starlark.errorf("%s", ex.getMessage());
---
+++ larky/src/main/java/com/verygood/security/larky/modules/types/results/Ok.java
@@ -0,0 +1,62 @@
+package com.verygood.security.larky.modules.types.results;
+
+import java.util.Objects;
+
+import net.starlark.java.eval.EvalException;
+import net.starlark.java.eval.Printer;
+
+public class Ok implements Result {
+
+  private final Object value;
+
+  Ok(Object value) {
+    this.value = value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Ok result = (Ok) o;
+    return Objects.equals(value, result.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getClass(), this.isOk(), value);
+  }
+
+  @Override
+  public String toString() {
+    return "Ok{" + value + '}';
+  }
+
+  @Override
+  public void str(Printer printer) {
+    printer.append(this.toString());
+  }
+
+  @Override
+  public Object getValue() {
+    return this.value;
+  }
+
+  @Override
+  public EvalException getError() {
+    return null;
+  }
+
+  @Override
+  public boolean isOk() {
+    return true;
+  }
+
+  @Override
+  public boolean isError() {
+    return false;
+  }
+}
