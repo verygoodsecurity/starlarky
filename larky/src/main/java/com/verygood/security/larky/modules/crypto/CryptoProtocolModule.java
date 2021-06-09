6f59cc6d28124e3975864bcfd290fe9b556cb0f7
Joshua Campbell <joshua.erik.campbell@gmail.com>
2021-06-09 10:37:52 -0700

    WIP PBKDF2

--- larky/src/main/java/com/verygood/security/larky/modules/crypto/CryptoProtocolModule.java
+++ larky/src/main/java/com/verygood/security/larky/modules/crypto/CryptoProtocolModule.java
@@ -33,6 +33,7 @@ public class CryptoProtocolModule implements StarlarkValue {
       @Param(name = "count", allowedTypes = { @ParamType(type = StarlarkInt.class)}, defaultValue = "1000"),
       @Param(name = "hashAlgo", allowedTypes = { @ParamType(type = String.class)}, defaultValue = "'SHA1'"),
   }, useStarlarkThread = true)
+
   public LarkyByteLike PBKDF1(LarkyByteLike password, LarkyByteLike salt, StarlarkInt dkLen, StarlarkInt count, String hashAlgo, StarlarkThread thread) throws EvalException {
 
     byte[] results = PBKDF1(
@@ -64,6 +65,38 @@ public class CryptoProtocolModule implements StarlarkValue {
     }
     PKCS5S1ParametersGenerator keygen = new PKCS5S1ParametersGenerator(digest);
     keygen.init(PKCS5S1ParametersGenerator.PKCS5PasswordToBytes(password), salt, count);
+    KeyParameter cipherParams = (KeyParameter) keygen.generateDerivedParameters(dkLen);
+    return cipherParams.getKey();
+  }
+
+  @StarlarkMethod(
+      name = "PBKDF2", parameters = {
+      @Param(name = "password", allowedTypes = { @ParamType(type = LarkyByteLike.class)}),
+      @Param(name = "salt", allowedTypes = { @ParamType(type = LarkyByteLike.class)}),
+      @Param(name = "dkLen", allowedTypes = { @ParamType(type = StarlarkInt.class)}, defaultValue = "16" ),
+      @Param(name = "count", allowedTypes = { @ParamType(type = StarlarkInt.class)}, defaultValue = "1000"),
+      @Param(name = "prf", allowedTypes = { @ParamType(type = LarkyByteLike.class)}, defaultValue = ),// need to determine proper param type for function?
+      @Param(name = "hmac_hash_module", allowedTypes = { @ParamType(type= String.class)})
+  }, useStarLarkThread = true)
+  public LarkyByteLike PBKDF2(LarkyByteLike password, LarkyByteLike salt, StarlarkInt dkLen, StarlarkInt count, String prf, Starlark Thread thread) throws EvalException {
+      byte [] results = PBKDF2(
+          password.toCharArray(),
+          salt.getBytes(),
+          dkLen.toIntUnchecked,
+          count.toIntUnchecked(),
+          prf,
+          hmac_hash_module);
+      return LarkyByteArray.builder(thread).setSequence(results).build();
+  }
+
+  @VisibleForTesting
+  byte[] PBKDF2(char[] password, byte[] salt, int dkLen_, int count, String prf, String hmac_hash_module) throws EvalException {
+    int dkLen = dkLen_ * BITS_IN_BYTES;
+    // first make sure only one of prf or hmac_hash_module were passed (mutually exclusive args)
+    // if neither passed, set hmac_hash_module as SHA1
+    // use prf of hmac module to create key of dklen
+    PKCS5S2ParametersGenerator keygen = new PKCS5S2ParametersGenerator();
+    keygen.init(PKCS5S2ParametersGenerator.PKCS5PasswordToBytes(password), salt, count);
     KeyParameter cipherParams = (KeyParameter) keygen.generateDerivedParameters(dkLen);
     return cipherParams.getKey();
   }
