217e00c27ccb3df34ba65e77a2dfa1bd7bda2e9d
Mohamad El Hajj <mohamad.h.elhajj@gmail.com>
2021-07-30 13:39:23 -0500

    remove lombok

--- larky-api/src/main/java/com/verygood/security/larkyapi/LarkyAPIEngine.java
+++ larky-api/src/main/java/com/verygood/security/larkyapi/LarkyAPIEngine.java
@@ -2,13 +2,11 @@ package com.verygood.security.larkyapi;
 
 import com.verygood.security.larky.jsr223.LarkyScriptEngine;
 import com.verygood.security.larky.parser.ParsedStarFile;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.script.ScriptContext;
 import javax.script.ScriptException;
 import java.io.Reader;
 
-@Slf4j
 public class LarkyAPIEngine extends LarkyScriptEngine {
 
     private Object extractStarfileOutput(ParsedStarFile starfile, String outputVar) {
