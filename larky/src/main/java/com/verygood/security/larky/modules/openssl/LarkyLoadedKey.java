c6b85da4e20a3b6e959142d2b699503e24887d31
Mahmoud Abdelkader <mabdelkader@gmail.com>
2021-10-22 13:12:05 -0700

    chore: Extract LarkyLoadedKey to a separate class.

--- larky/src/main/java/com/verygood/security/larky/modules/openssl/LarkyLoadedKey.java
+++ larky/src/main/java/com/verygood/security/larky/modules/openssl/LarkyLoadedKey.java
@@ -2,6 +2,7 @@ package com.verygood.security.larky.modules.openssl;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.interfaces.DSAKey;
 import java.security.interfaces.ECKey;
 import java.security.interfaces.RSAKey;
@@ -29,41 +30,61 @@ public class LarkyLoadedKey implements StarlarkValue {
     }
   }
 
-  final private KeyPair kp;
+  public static KEY_TYPE keyType(PrivateKey aPrivate) {
+     if (aPrivate instanceof RSAKey) {
+           return KEY_TYPE.RSA;
+         } else if (aPrivate instanceof DSAKey) {
+       return KEY_TYPE.DSA;
+         } else if (aPrivate instanceof ECKey) {
+       return KEY_TYPE.ECKey;
+         } else {
+       return KEY_TYPE.UNKNOWN;
+         }
+   }
+
+  final private PrivateKey privateKey;
+  final private PublicKey publicKey;
   final private KEY_TYPE type;
 
-  public LarkyLoadedKey(KeyPair kp) {
-    this.kp = kp;
-    PrivateKey key = kp.getPrivate();
-    if (key instanceof RSAKey) {
-      type = KEY_TYPE.RSA;
-    } else if (key instanceof DSAKey) {
-      type = KEY_TYPE.DSA;
-    } else if (kp.getPrivate() instanceof ECKey) {
-      type = KEY_TYPE.ECKey;
-    } else {
-      type = KEY_TYPE.UNKNOWN;
-    }
+  public LarkyLoadedKey(PublicKey aPublic, PrivateKey aPrivate, KEY_TYPE type) {
+    this.privateKey = aPrivate;
+    this.publicKey = aPublic;
+    this.type = type;
   }
 
+  public LarkyLoadedKey(PublicKey aPublic, PrivateKey aPrivate) {
+   this(aPublic, aPrivate, keyType(aPrivate));
+  }
+
+  public LarkyLoadedKey(KeyPair kp) {
+    this(kp.getPublic(), kp.getPrivate());
+  }
+
+  public PrivateKey getPrivateKey() {
+    return privateKey;
+  }
+
+  public PublicKey getPublicKey() {
+    return publicKey;
+  }
+
+
   @StarlarkMethod(name = "pkey", structField = true)
-  public StarlarkBytes loadPrivateKey() throws EvalException {
-    return StarlarkBytes.immutableOf(kp.getPrivate().getEncoded());
-//      return StarlarkBytes.builder(null).setSequence(kp.getPrivate().getEncoded()).build();
+  public StarlarkBytes loadPrivateKey() {
+    return StarlarkBytes.immutableOf(getPrivateKey().getEncoded());
   }
 
   @StarlarkMethod(name = "bits", structField = true)
   public StarlarkInt bits() throws EvalException {
     switch (type) {
       case RSA:
-        return StarlarkInt.of(((RSAKey) kp.getPrivate()).getModulus().bitLength());
+        return StarlarkInt.of(((RSAKey)getPrivateKey()).getModulus().bitLength());
       case DSA:
-        return StarlarkInt.of(((DSAKey) kp.getPrivate()).getParams().getP().bitLength());
+        return StarlarkInt.of(((DSAKey)getPrivateKey()).getParams().getP().bitLength());
       case ECKey:
-        return StarlarkInt.of(((ECKey) kp.getPrivate()).getParams().getCurve().getField().getFieldSize());
+        return StarlarkInt.of(((ECKey) getPrivateKey()).getParams().getCurve().getField().getFieldSize());
       default:
         throw new EvalException("Unable to determine length in bits of specified Key instance");
-
     }
   }
 
