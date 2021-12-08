3ff7f4eb9b43caf81729d30559a5477eace59f1e
La Luo <la.luo@verygoodsecurity.com>
2021-12-07 16:17:26 -0800

    enrich and catogorize docs

--- docs_poc/source/conf.py
+++ docs_poc/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(1, os.path.abspath(os.path.join('..', 'examples')))
+sys.path.insert(1, os.path.abspath(os.path.join('../..', 'larky/src/main/resources/stdlib')))
 
 
 # -- Project information -----------------------------------------------------
--- docs_poc/source/stdlib.rst
+++ docs_poc/source/stdlib.rst
@@ -3,11 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Starlark Docs POC
+Starlark Standard Lib
 =================
 
-.. toctree::
-   :maxdepth: 2
-
-   vault
-   stdlib
+.. automodule:: math
+   :members:
