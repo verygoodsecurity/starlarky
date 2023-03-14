f1df79897ef29a46779a8ed582319bd0f3573df7
Fang-Pen Lin <hello@fangpenlin.com>
2023-03-14 10:44:27 -0700

    New approach

--- larky/src/main/java/com/verygood/security/larky/modules/NetworkTokenModule.java
+++ larky/src/main/java/com/verygood/security/larky/modules/NetworkTokenModule.java
@@ -1,29 +1,20 @@
 package com.verygood.security.larky.modules;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Maps;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.PathNotFoundException;
 import com.verygood.security.larky.modules.vgs.nts.LarkyNetworkToken;
 import com.verygood.security.larky.modules.vgs.nts.MockNetworkTokenService;
 import com.verygood.security.larky.modules.vgs.nts.NoopNetworkTokenService;
 import com.verygood.security.larky.modules.vgs.nts.spi.NetworkTokenService;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 import net.starlark.java.annot.Param;
 import net.starlark.java.annot.ParamType;
 import net.starlark.java.annot.StarlarkBuiltin;
 import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.NoneType;
 import net.starlark.java.eval.Starlark;
-import net.starlark.java.eval.StarlarkInt;
-import net.starlark.java.eval.StarlarkList;
 import net.starlark.java.eval.StarlarkThread;
 
 @StarlarkBuiltin(name = "nts", category = "BUILTIN", doc = "Overridable Network Token API in Larky")
@@ -54,178 +45,35 @@ public class NetworkTokenModule implements LarkyNetworkToken {
   }
 
   @StarlarkMethod(
-      name = "render",
-      doc =
-          "Get network token and the cryptogram for given pan alias and inject the network token values into the original input and return",
+      name = "get_network_token",
+      doc = "Get network token and the cryptogram for given pan alias and return",
       useStarlarkThread = true,
       parameters = {
         @Param(
-            name = "input",
-            doc = "Input json payload",
-            named = true,
-            allowedTypes = {@ParamType(type = Object.class)}),
-        @Param(
             name = "pan",
             named = true,
-            doc =
-                "JSONPath to the PAN alias in the input payload for looking up the corresponding network token to be used and replace the original value at given JSONPath",
+            doc = "then pan alias value for looking network token up",
             allowedTypes = {@ParamType(type = String.class)}),
-        @Param(
-            name = "exp_month",
-            named = true,
-            defaultValue = "None",
-            doc =
-                "JSONPath to insert the expire month from network token to the input payload and return",
-            allowedTypes = {@ParamType(type = String.class), @ParamType(type = NoneType.class)}),
-        @Param(
-            name = "exp_year",
-            named = true,
-            defaultValue = "None",
-            doc =
-                "JSONPath to insert the expire year from network token to the input payload and return",
-            allowedTypes = {@ParamType(type = String.class), @ParamType(type = NoneType.class)}),
-        @Param(
-            name = "cryptogram_value",
-            named = true,
-            defaultValue = "None",
-            doc =
-                "JSONPath to insert the cryptogram value from network token to the input payload and return",
-            allowedTypes = {@ParamType(type = String.class), @ParamType(type = NoneType.class)}),
-        @Param(
-            name = "cryptogram_eci",
-            named = true,
-            defaultValue = "None",
-            doc =
-                "JSONPath to insert the cryptogram eci from network token to the input payload and return",
-            allowedTypes = {@ParamType(type = String.class), @ParamType(type = NoneType.class)}),
       })
   @Override
-  public Object render(
-      Object input,
-      String pan,
-      Object expireMonth,
-      Object expireYear,
-      Object cryptogramValue,
-      Object cryptogramEci,
-      StarlarkThread thread)
+  public Dict<String, Object> getNetworkToken(String pan, StarlarkThread thread)
       throws EvalException {
-    final Object jsonPayload = fromStarlark(input);
-    final DocumentContext context = JsonPath.parse(jsonPayload);
-
-    final String panAlias;
-    try {
-      panAlias = context.read(pan);
-    } catch (PathNotFoundException exception) {
-      throw Starlark.errorf("pan JSONPath \"%s\" does not exist", pan);
-    }
-    if (panAlias == null || panAlias.trim().isEmpty()) {
-      throw Starlark.errorf("pan argument is required");
-    }
-
     final Optional<NetworkTokenService.NetworkToken> networkTokenOptional;
     try {
-      networkTokenOptional = networkTokenService.getNetworkToken(panAlias);
+      networkTokenOptional = networkTokenService.getNetworkToken(pan);
     } catch (UnsupportedOperationException exception) {
-      throw Starlark.errorf("nts.render's getNetworkToken operation must be overridden");
+      throw Starlark.errorf("nts.get_network_token operation must be overridden");
     }
     if (!networkTokenOptional.isPresent()) {
       throw Starlark.errorf("network token does not found");
     }
     final NetworkTokenService.NetworkToken networkToken = networkTokenOptional.get();
-
-    // Map from JSON path to its corresponding value to insert into the input JSON payload
-    final ImmutableList<Map.Entry<Optional<String>, Object>> valuePlacements =
-        ImmutableList.<Map.Entry<Optional<String>, Object>>builder()
-            .add(Maps.immutableEntry(Optional.of(pan), networkToken.getToken()))
-            .add(Maps.immutableEntry(optionalValue(expireYear), networkToken.getExpireYear()))
-            .add(Maps.immutableEntry(optionalValue(expireMonth), networkToken.getExpireMonth()))
-            .add(
-                Maps.immutableEntry(
-                    optionalValue(cryptogramValue), networkToken.getCryptogramValue()))
-            .add(Maps.immutableEntry(optionalValue(cryptogramEci), networkToken.getCryptogramEci()))
-            .build();
-    // Set values for each JSON path and value pairs to the output JSON payload
-    try {
-      valuePlacements.stream()
-          // We are only interested in making insertions for present JSONPaths
-          .filter(keyValue -> keyValue.getKey().isPresent())
-          .forEach(
-              keyValue -> {
-                final String jsonPath = keyValue.getKey().get();
-                try {
-                  context.set(jsonPath, keyValue.getValue());
-                } catch (final PathNotFoundException exception) {
-                  throw new RuntimeException(
-                      Starlark.errorf(
-                          "JSONPath \"%s\" does not exist, if you want to insert value to a nested path, please ensure "
-                              + "that the target value already exists at the given path in the input",
-                          jsonPath));
-                }
-              });
-    } catch (final RuntimeException exception) {
-      if (exception.getCause() instanceof EvalException) {
-        throw (EvalException) exception.getCause();
-      }
-      throw exception;
-    }
-    return toStarlark(thread, context.json());
-  }
-
-  // Convert sparkly JSON object into ordinary Java JSON object
-  private static Object fromStarlark(Object object) {
-    if (object instanceof Dict) {
-      return fromStarlarkDict((Dict) object);
-    }
-    if (object instanceof StarlarkList) {
-      return fromStarlarkList((StarlarkList) object);
-    }
-    return object;
-  }
-
-  private static Map<Object, Object> fromStarlarkDict(Dict<Object, Object> dict) {
-    return dict.entrySet().stream()
-        .map(
-            entry ->
-                Maps.immutableEntry(fromStarlark(entry.getKey()), fromStarlark(entry.getValue())))
-        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-  }
-
-  private static List<Object> fromStarlarkList(StarlarkList<Object> list) {
-    return list.stream().map(value -> fromStarlark(value)).collect(Collectors.toList());
-  }
-
-  private static Object toStarlark(StarlarkThread thread, Object object) {
-    if (object instanceof Map) {
-      return toStarlarkDict(thread, (Map<Object, Object>) object);
-    }
-    if (object instanceof List) {
-      return toStarlarkList(thread, (List<Object>) object);
-    }
-    if (object instanceof Integer) {
-      return StarlarkInt.of((Integer) object);
-    }
-    return object;
-  }
-
-  private static Dict<Object, Object> toStarlarkDict(
-      StarlarkThread thread, Map<Object, Object> map) {
-    final Map<Object, Object> convertedMap =
-        map.entrySet().stream()
-            .map(
-                entry ->
-                    Maps.immutableEntry(
-                        toStarlark(thread, entry.getKey()), toStarlark(thread, entry.getValue())))
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    return Dict.copyOf(thread.mutability(), convertedMap);
-  }
-
-  private static StarlarkList<Object> toStarlarkList(StarlarkThread thread, List<Object> list) {
-    final List<Object> convertedList =
-        list.stream().map(value -> toStarlark(thread, value)).collect(Collectors.toList());
-    return StarlarkList.copyOf(thread.mutability(), convertedList);
-  }
-
-  private static <T> Optional<T> optionalValue(Object obj) {
-    return obj == Starlark.NONE ? Optional.empty() : Optional.of((T) obj);
+    return Dict.<String, Object>builder()
+        .put("token", networkToken.getToken())
+        .put("exp_month", networkToken.getExpireMonth())
+        .put("exp_year", networkToken.getExpireYear())
+        .put("cryptogram_value", networkToken.getCryptogramValue())
+        .put("cryptogram_eci", networkToken.getCryptogramEci())
+        .build(thread.mutability());
   }
 }
--- larky/src/main/java/com/verygood/security/larky/modules/vgs/nts/LarkyNetworkToken.java
+++ larky/src/main/java/com/verygood/security/larky/modules/vgs/nts/LarkyNetworkToken.java
@@ -1,31 +1,18 @@
 package com.verygood.security.larky.modules.vgs.nts;
 
+import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.EvalException;
 import net.starlark.java.eval.StarlarkThread;
 import net.starlark.java.eval.StarlarkValue;
 
 public interface LarkyNetworkToken extends StarlarkValue {
   /**
-   * Input JSON payload object, get the network token by given PAN alias and replace the input JSON
-   * payload object values by given JSONPaths for each token properties and return.
+   * Get network token by pan alias.
    *
-   * @param input Input JSON payload object
-   * @param pan JSONPath to the PAN alias value in the input JSON payload
-   * @param expireMonth JSONPath to the expiring month to insert in the result JSON payload object
-   * @param expireYear JSONPath to the expiring year to insert in the result JSON payload object
-   * @param cryptogramValue JSONPath to the cryptogram value to insert in the result JSON payload
-   *     object
-   * @param cryptogramEci JSONPath to the cryptogram ECI to insert in the result JSON payload object
+   * @param pan the pan alias value for getting network token
    * @param thread Starlark thread object
-   * @return
+   * @return a dict contains the network token values
    */
-  Object render(
-      Object input,
-      String pan,
-      Object expireMonth,
-      Object expireYear,
-      Object cryptogramValue,
-      Object cryptogramEci,
-      StarlarkThread thread)
+  Dict<String, Object> getNetworkToken(String pan, StarlarkThread thread)
       throws EvalException;
 }
