7e809716ccef59caf98da30dac6f16371020100f
Andrew Rosenberg <2112318+Iapetus999@users.noreply.github.com>
2024-02-12 12:41:50 -0800

    Update Python, JVM in build (#454)
    * Update Python, JVM in build
    
    * Fix local Python tests

--- build-and-test-python.sh
+++ build-and-test-python.sh
@@ -1,4 +1,12 @@
-pyenv local 3.8.6
+set -ex
+
+pyenv local 3.10.5
+PATH=${PATH}:~/.local/bin
+
+# You must run ./build-and-test-java.sh to build this
+cp ./runlarky/target/larky-runner ./pylarky
+
+# Run tests
 poetry version ${VERSION}
 poetry install
 poetry run pytest pylarky/tests
