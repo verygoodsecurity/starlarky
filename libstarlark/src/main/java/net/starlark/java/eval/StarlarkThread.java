024d2e0378aab7ac97589a3a8bcf0951399cbf6b
Andrew Rosenberg <2112318+Iapetus999@users.noreply.github.com>
2024-03-07 13:14:53 -0800

    Revert the ticks (#466)

--- libstarlark/src/main/java/net/starlark/java/eval/StarlarkThread.java
+++ libstarlark/src/main/java/net/starlark/java/eval/StarlarkThread.java
@@ -20,7 +20,7 @@ import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 import net.starlark.java.syntax.Location;
@@ -56,7 +56,7 @@ public final class StarlarkThread {
   // not in the corresponding pop, but when the last frame is popped, because
   // the profiler session might start in the middle of a call and/or run beyond
   // the lifetime of this thread.
-  final AtomicLong cpuTicks = new AtomicLong();
+  final AtomicInteger cpuTicks = new AtomicInteger();
   @Nullable private CpuProfiler profiler;
   private StarlarkThread savedThread; // saved StarlarkThread, when profiling reentrant evaluation
 
@@ -76,8 +76,6 @@ public final class StarlarkThread {
   public long getExecutedSteps() {
     return steps;
   }
-
-  public long getCpuTicks() { return cpuTicks.get();}
 
   /**
    * Sets the maximum number of Starlark computation steps that may be executed by this thread (see
@@ -260,7 +258,7 @@ public final class StarlarkThread {
     Frame fr = callstack.get(last);
 
     if (profiler != null) {
-      long ticks = cpuTicks.getAndSet(0);
+      int ticks = cpuTicks.getAndSet(0);
       if (ticks > 0) {
         profiler.addEvent(ticks, getDebugCallStack());
       }
