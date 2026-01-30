d618d8f769bc8a157d463842a3f66f403f2fb330
viktor.kotziuba <viktor.kotziuba@vgs.io>
2026-01-30 18:49:07 +0200

    fix(SD-4274): gha migration

--- install_graalvm.sh
+++ install_graalvm.sh
@@ -3,13 +3,31 @@
 set -e
 
 cd "$(dirname "$0")"
-GRAALVM_HOME=${GRAALVM_HOME:=./.graalvm}
+GRAALVM_DIR=${GRAALVM_DIR:=./.graalvm}
 
-if [ ! -d "$GRAALVM_HOME" ]; then
+OS_TYPE=${OS_TYPE:-$(uname)}
+if [ "$OS_TYPE" == "Darwin" ]; then
+    # For local development. You can use it, but it's at your own risk.
+    echo "macOS detected."
+    GRAALVM_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.3.0/graalvm-ce-java11-darwin-amd64-20.3.0.tar.gz"
+    GRAALVM_PACKAGE="graalvm-ce-java11-20.3.0"
+    GRAALVM_BIN=${GRAALVM_DIR}/Contents/Home/bin
+elif [ "$OS_TYPE" == "Linux" ]; then
+    # For CI/CD builds and MakeFile usage
+    echo "Linux detected."
+    GRAALVM_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-20.0.2/graalvm-community-jdk-20.0.2_linux-x64_bin.tar.gz"
+    GRAALVM_PACKAGE="graalvm-community-openjdk-20.0.2+9.1"
+    GRAALVM_BIN=${GRAALVM_DIR}/bin
+else
+    echo "Unsupported OS: $OS_TYPE"
+    exit 1
+fi
+
+if [ ! -d "$GRAALVM_DIR" ]; then
   echo "GraalVM exists in '$(pwd)'. Updating..."
-  curl https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-20.0.2/graalvm-community-jdk-20.0.2_linux-x64_bin.tar.gz -O -J -L
-  tar xfz graalvm-community-jdk-20.0.2_linux-x64_bin.tar.gz
-  mv graalvm-community-openjdk-20.0.2+9.1 .graalvm
-  rm graalvm-community-jdk-20.0.2_linux-x64_bin.tar.gz
-  $GRAALVM_HOME/bin/gu install native-image
+  curl -o graalvm.tar.gz -J -L "$GRAALVM_URL"
+  tar xfz graalvm.tar.gz
+  mv $GRAALVM_PACKAGE .graalvm
+  rm graalvm.tar.gz
+  $GRAALVM_BIN/gu install native-image
 fi
