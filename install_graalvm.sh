d4db990aced828cdd43260e32150d3ba9c586bb0
viktor.kotziuba <viktor.kotziuba@vgs.io>
2026-01-30 20:52:37 +0200

    fix(SD-4274): gha migration

--- install_graalvm.sh
+++ install_graalvm.sh
@@ -7,10 +7,10 @@ GRAALVM_DIR=${GRAALVM_DIR:=./.graalvm}
 
 OS_TYPE=${OS_TYPE:-$(uname)}
 if [ "$OS_TYPE" == "Darwin" ]; then
-    # For local development. You can use it, but it's at your own risk.
+    # For local development.
     echo "macOS detected."
-    GRAALVM_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.3.0/graalvm-ce-java11-darwin-amd64-20.3.0.tar.gz"
-    GRAALVM_PACKAGE="graalvm-ce-java11-20.3.0"
+    GRAALVM_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-20.0.2/graalvm-community-jdk-20.0.2_macos-aarch64_bin.tar.gz"
+    GRAALVM_PACKAGE="graalvm-community-openjdk-20.0.2+9.1"
     GRAALVM_BIN=${GRAALVM_DIR}/Contents/Home/bin
 elif [ "$OS_TYPE" == "Linux" ]; then
     # For CI/CD builds and MakeFile usage
