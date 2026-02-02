286b7b6e39c9fcc2ccadc8fcc39d202ae7d47851
viktor.kotziuba <viktor.kotziuba@vgs.io>
2026-02-02 15:41:06 +0200

    fix(SD-4274): gha migration

--- build-and-test-java.sh
+++ build-and-test-java.sh
@@ -3,22 +3,9 @@ cd "$(dirname "$0")"
 
 OS_TYPE=${OS_TYPE:-$(uname)}
 OS_TYPE_LOWER=$(echo "$OS_TYPE" | tr '[:upper:]' '[:lower:]')
-TEST_RESULTS_PATH=${TEST_RESULTS_PATH:-/tmp/test-results}
 
-mkdir -p $TEST_RESULTS_PATH/junit/
-mkdir -p $TEST_RESULTS_PATH/coverage/
-mvn clean install dependency:go-offline -T 2.0C -B
-find . -type f -regex ".*/target/surefire-reports/.*xml" -exec cp {} $TEST_RESULTS_PATH/junit/ \;
-find . -type f -regex ".*/target/surefire-reports/.*-output.txt" -exec cp {} $TEST_RESULTS_PATH/junit/ \;
-find . -type f -regex ".*/target/site/.*" -exec cp --parents {} $TEST_RESULTS_PATH/coverage/ \;
-
-# package it up to deliver
+# test and package
 mvn package -Pnative
-# TODO remove: when CircleCI is disabled
-DIST_PATH=${DIST_PATH:-./dist}
-mkdir ${DIST_PATH}
-cp ./runlarky/target/larky-runner ${DIST_PATH}/larky-linux
-# cp dist/*.whl ${DIST_PATH}
 
 # tag distribution
 mv ./runlarky/target/larky-runner ./runlarky/target/larky-${OS_TYPE_LOWER}
