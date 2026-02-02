9960255244cec15d2e807269eb288f28ab6894a2
viktor.kotziuba <viktor.kotziuba@vgs.io>
2026-02-02 16:11:04 +0200

    fix(SD-4274): gha migration

--- build-and-test-python.sh
+++ build-and-test-python.sh
@@ -9,7 +9,7 @@ VERSION=${VERSION:-0.0.1}
 
 # You must run ./build-and-test-java.sh to build this
 cp ./runlarky/target/larky-${OS_TYPE_LOWER} ./pylarky/larky-runner
-chmod +x ./pylarky/larky-${OS_TYPE_LOWER}
+chmod +x ./pylarky/larky-runner
 
 # Run tests
 poetry version ${VERSION}
